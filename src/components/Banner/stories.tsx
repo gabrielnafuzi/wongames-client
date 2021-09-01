4de1e8b08ead59f48a6496d1ea9ad0eb74a1b14a
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-09-01 18:19:33 -0300

    feat: add Banner component

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,37 @@
+import { Story, Meta } from '@storybook/react'
+import { Banner, BannerProps } from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story<BannerProps> = (args) => (
+  <div
+    style={{
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}
+  >
+    <div
+      style={{
+        width: '100%',
+        padding: '3rem'
+      }}
+    >
+      <Banner {...args} />
+    </div>
+  </div>
+)
---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,38 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests'
+
+import { Banner } from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /play the new crashlands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /defy death/i })).toHaveAttribute(
+      'src',
+      'https://source.unsplash.com/user/willianjusten/1042x580'
+    )
+    expect(screen.getByRole('img', { name: /defy death/i })).toHaveAttribute(
+      'aria-label',
+      'Defy death'
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
