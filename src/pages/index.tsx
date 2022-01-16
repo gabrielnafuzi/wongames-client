64f12abba8e5701e0f5eb998ad86dd1717d3e9f8
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-01-16 03:19:24 -0300

    add types to getServerSideProps on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
-import { Home } from '@/templates'
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 
-const Index = (props) => {
-  return <Home {...props} />
-}
+import { items as bannersMock } from '@/components/BannerSlider/mock'
+import { items as gamesMock } from '@/components/GameCardSlider/mock'
+import { item as highlightMock } from '@/components/Highlight/mock'
+import { Home, HomeTemplateProps } from '@/templates'
 
-export const getStaticProps = () => {
-  return {
-    props: {
-      heading: 'Olha eu aqui'
+const Index: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> =
+  ({ children: _, ...props }) => {
+    return <Home {...props} />
+  }
+
+export const getServerSideProps: GetServerSideProps<HomeTemplateProps> =
+  async () => {
+    return {
+      props: {
+        banners: bannersMock,
+        newGamers: gamesMock,
+        mostPopularHighlight: highlightMock,
+        mostPopularGames: gamesMock,
+        upcomingGames: gamesMock,
+        upcomingHighlight: highlightMock,
+        upcomingMoreGames: gamesMock,
+        freeGames: gamesMock,
+        freeHighlight: highlightMock
+      }
     }
   }
-}
 
 export default Index
