6572fe8a1cf462d959b899ca70c21944241bc38f
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-12 03:53:50 -0300

    feat: add onCheck and onChange methods

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,23 +1,46 @@
+import { InputHTMLAttributes, useState } from 'react'
+
 import * as S from './styles'
 
 export type CheckboxProps = {
+  onCheck?: (status: boolean) => void
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
-}
+} & InputHTMLAttributes<HTMLInputElement>
 
 export const Checkbox = ({
   label,
   labelFor = '',
-  labelColor = 'white'
-}: CheckboxProps) => (
-  <S.Wrapper>
-    <S.Input id={labelFor} type="checkbox" />
+  labelColor = 'white',
+  onCheck,
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(false)
 
-    {!!label && (
-      <S.Label htmlFor={labelFor} labelColor={labelColor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+  const onChange = () => {
+    const status = !checked
+
+    setChecked(status)
+
+    if (onCheck) {
+      onCheck(status)
+    }
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+      />
+
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
