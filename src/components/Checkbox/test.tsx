9fcc0a94858e2532eaff7d4b850aa085337bb507
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-12 03:13:21 -0300

    test: ensure checkbox will render correctly

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from '@/utils/tests'
+
+import { Checkbox } from '.'
+
+describe('<Checkbox />', () => {
+  it('should render with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+})
