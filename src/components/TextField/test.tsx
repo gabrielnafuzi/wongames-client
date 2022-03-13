1795b4ad2d83e390c05ccf969a0c5d694e3f3d06
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-13 00:33:04 -0300

    test: ensure textfield render an icon when passed

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -1,3 +1,4 @@
+import { Email } from '@styled-icons/material-outlined'
 import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
@@ -53,5 +54,11 @@ describe('<TextField />', () => {
     userEvent.tab()
 
     expect(input).toHaveFocus()
+  })
+
+  it('should render with a icon', () => {
+    renderWithTheme(<TextField icon={<Email data-testid="icon" />} />)
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 })
