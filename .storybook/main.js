2a937966075395db6b9ac51568f8b52c826e6567
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-13 01:10:48 -0300

    chore: add dark mode to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,18 +3,22 @@ const path = require('path')
 
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
+  addons: [
+    '@storybook/addon-essentials',
+    'storybook-addon-next-router',
+    'storybook-dark-mode',
+  ],
   babel: async (options) => ({
     ...options,
     plugins: [
       ...options.plugins,
-      require.resolve('@babel/plugin-transform-react-jsx')
-    ]
+      require.resolve('@babel/plugin-transform-react-jsx'),
+    ],
   }),
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     config.resolve.alias['@'] = path.resolve(__dirname, '../src')
 
     return config
-  }
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,26 +1,32 @@
 import { RouterContext } from 'next/dist/next-server/lib/router-context'
 
+import { themes } from '@storybook/theming'
 import { ThemeProvider } from 'styled-components'
 
 import { GlobalStyles, theme } from '@/styles'
 
 export const parameters = {
   nextRouter: {
-    Provider: RouterContext.Provider
+    Provider: RouterContext.Provider,
   },
   backgrounds: {
     default: 'won-light',
     values: [
       {
         name: 'won-light',
-        value: theme.colors.white
+        value: theme.colors.white,
       },
       {
         name: 'won-dark',
-        value: theme.colors.mainBg
-      }
-    ]
-  }
+        value: theme.colors.mainBg,
+      },
+    ],
+  },
+  darkMode: {
+    dark: { ...themes.dark },
+    light: { ...themes.normal },
+    current: 'dark',
+  },
 }
 
 export const decorators = [
@@ -29,5 +35,5 @@ export const decorators = [
       <GlobalStyles removeBg />
       <Story />
     </ThemeProvider>
-  )
+  ),
 ]
