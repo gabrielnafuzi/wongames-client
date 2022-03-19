c0dd2872e3f547cfd484045e753393e152c8aa2f
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-19 11:41:07 -0300

    feat: add desktop styles

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -13,26 +13,33 @@ export const Auth = ({ children, title }: AuthProps) => {
   return (
     <S.Wrapper>
       <S.BannerBlock>
-        <Logo />
+        <S.BannerContent>
+          <Logo />
 
-        <Heading>All your favorite games in one place</Heading>
-        <S.Subtitle>
-          <strong>WON</strong> is the best and most complete gaming platform.
-        </S.Subtitle>
+          <div>
+            <Heading>All your favorite games in one place</Heading>
+            <S.Subtitle>
+              <strong>WON</strong> is the best and most complete gaming
+              platform.
+            </S.Subtitle>
+          </div>
 
-        <S.Footer>
-          Won Games {currentYear} © Todos os direitos reservados
-        </S.Footer>
+          <S.Footer>
+            Won Games {currentYear} © Todos os direitos reservados
+          </S.Footer>
+        </S.BannerContent>
       </S.BannerBlock>
 
       <S.Content>
-        <Logo color="black" size="large" />
+        <S.ContentWrapper>
+          <Logo color="black" size="large" />
 
-        <Heading color="black" lineColor="secondary" lineLeft>
-          {title}
-        </Heading>
+          <Heading color="black" lineColor="secondary" lineLeft>
+            {title}
+          </Heading>
 
-        {children}
+          {children}
+        </S.ContentWrapper>
       </S.Content>
     </S.Wrapper>
   )
