e4caba39d1d38067b8c600b60725db02312d5c2f
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-19 11:20:50 -0300

    test: ensure components and children are rendering

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,13 +1,45 @@
-import { render, screen } from '@testing-library/react'
+import 'match-media-mock'
+
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from '@/utils/tests'
 
 import { Auth } from '.'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Auth />)
+  it('should render all components and children', () => {
+    const currentYear = new Date().getFullYear()
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>
+    )
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getAllByRole('img')).toHaveLength(2)
+
+    expect(
+      screen.getByRole('heading', {
+        name: /all your favorite games in one place/i,
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /auth title/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform/i,
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+
+    expect(
+      screen.getByText(
+        `Won Games ${currentYear} © Todos os direitos reservados`
+      )
+    ).toBeInTheDocument()
   })
 })
