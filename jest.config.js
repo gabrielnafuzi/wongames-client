408a037c755bde909c9d9d13a7dec38079fc5971
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-04-05 20:18:46 -0300

    feat: add form signin

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  }
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 }
