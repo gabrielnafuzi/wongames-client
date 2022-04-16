386149234d8661bd989264268138b17c22f28b9f
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-04-16 02:44:39 -0300

    refactor: change tests name

--- src/hooks/use-disclosure/use-disclosure.spec.ts
+++ src/hooks/use-disclosure/use-disclosure.spec.ts
@@ -3,7 +3,7 @@ import { renderHook, act } from '@testing-library/react'
 import { useDisclosure } from '.'
 
 describe('useDisclosure', () => {
-  it('should close correctly', () => {
+  it('should handle close correctly', () => {
     const { result } = renderHook(() => useDisclosure(true))
     expect(result.current[0]).toBe(true)
 
@@ -11,7 +11,7 @@ describe('useDisclosure', () => {
     expect(result.current[0]).toBe(false)
   })
 
-  it('should open correctly', () => {
+  it('should handle open correctly', () => {
     const { result } = renderHook(() => useDisclosure(false))
     expect(result.current[0]).toBe(false)
 
@@ -19,7 +19,7 @@ describe('useDisclosure', () => {
     expect(result.current[0]).toBe(true)
   })
 
-  it('should toggle correctly', () => {
+  it('should handle toggle correctly', () => {
     const { result } = renderHook(() => useDisclosure(false))
     expect(result.current[0]).toBe(false)
 
