f888490b033bcda0108bdb8bc4b3aac0dc9f5bba
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-04-22 17:11:32 -0300

    chore: add template option to plop

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -5,30 +5,58 @@ module.exports = (plop) => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your component name?'
-      }
+        message: 'What is your component name?',
+      },
     ],
     actions: [
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/index.tsx',
-        templateFile: 'templates/Component.tsx.hbs'
+        templateFile: 'templates/Component.tsx.hbs',
       },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/styles.ts',
-        templateFile: 'templates/styles.ts.hbs'
+        templateFile: 'templates/styles.ts.hbs',
       },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
+        templateFile: 'templates/stories.tsx.hbs',
       },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/test.tsx',
-        templateFile: 'templates/test.tsx.hbs'
-      }
-    ]
+        templateFile: 'templates/test.tsx.hbs',
+      },
+    ],
+  })
+
+  plop.setGenerator('template', {
+    description: 'Create a template',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your template name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/Component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs',
+      },
+    ],
   })
 }
